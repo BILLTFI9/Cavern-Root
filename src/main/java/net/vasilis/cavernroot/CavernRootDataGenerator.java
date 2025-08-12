9a7c398358bee8e79b6f806395c4d4b445f90dba
BILLTFI9 <gamingbllltfi@gmail.com>
2025-08-13 00:17:03 +0300

    August 12 2025 Commit 2

--- src/main/java/net/vasilis/cavernroot/CavernRootDataGenerator.java
+++ src/main/java/net/vasilis/cavernroot/CavernRootDataGenerator.java
@@ -13,12 +13,8 @@ public class CavernRootDataGenerator implements DataGeneratorEntrypoint {
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
-		pack.addProvider(ModBlockTagProvider::new);
-		pack.addProvider(ModItemTagProvider::new);
-		pack.addProvider(ModLootTableProvider::new);
-		pack.addProvider(ModModelProvider::new);
-		pack.addProvider(ModRecipeProvider::new);
 		pack.addProvider(ModRegistryDataGenerator::new);
+
 	}
 
 	@Override
--- src/main/java/net/vasilis/cavernroot/item/ModFoodComponents.java
+++ src/main/java/net/vasilis/cavernroot/item/ModFoodComponents.java
@@ -1,10 +1,10 @@
 package net.vasilis.cavernroot.item;
 
-import net.minecraft.component.type.FoodComponent;
+import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent TWISTED_CAVERN_ROOT = new FoodComponent.Builder()
-            .nutrition(3)
+            .hunger(3)
             .saturationModifier(0.4F)
             .snack()
             .build();
